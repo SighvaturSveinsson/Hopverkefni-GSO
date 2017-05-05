d6d85d09e8ab94498338e540f0c109e7b90d2868
davidercool <DBjarki@gmail.com>
2017-05-05 12:02:21 +0200

    Add files via upload

--- Lokaverkefni Töflur.sql
+++ Lokaverkefni Töflur.sql
@@ -5,8 +5,8 @@
     ID INT primary key auto_increment,
     Nafn Varchar(100),
     Utgafuar int,
-    ID_tegundar int auto_increment,
-    ID_utgefanda int auto_increment,
+    ID_tegundar int,
+    ID_utgefanda int,
     foreign key(ID_tegundar) references tegund(ID),
 	foreign key(ID_utgefanda) references útgefandi(ID)
     );
@@ -24,7 +24,7 @@
 	create table lag (
     ID INT primary key auto_increment,
     Nafn Varchar(100),
-    Lengd Varchar(10),
+    Lengd int,
     ID_flytjanda int,
     ID_hofundar int,
     ID_disks int,
@@ -32,6 +32,7 @@
     foreign key(ID_hofundar) references lagahöfundur(ID),
 	foreign key(ID_disks) references diskur(ID)
     );
+    
 	create table lagahöfundur (
     ID INT primary key auto_increment,
     Nafn Varchar(100),
@@ -47,7 +48,7 @@
 	Aldur int,
     Faedingardagur int,
     Dánardagur int,
-    ID_flokks int auto_increment,
+    ID_flokks int,
     foreign key(ID_flokks) references flokkur(ID)
     );
     
@@ -63,9 +64,7 @@ VALUES
 ("Equal Vision Records"),
 ("Reprise Records"),
 ("Capitol Records"),
-("Columbia Records"),
-("Evil Ink"),
-("Columbia Records");
+("Evil Ink");
 
 INSERT INTO tegund (nafn)
 VALUES 
@@ -73,29 +72,22 @@ VALUES
 ("Symphonic rock"),
 ("Punk rock"),
 ("Heavy metal"),
-("Progressive rock"),
-("Alternative rock"),
 ("Progressive rock");
 
 INSERT INTO flokkur (nafn)
 VALUES 
 ("Hljómsveit"),
-("Solo"),
-("Hljómsveit"),
-("Hljómsveit"),
-("Hljómsveit"),
-("Hljómsveit"),
-("Hljómsveit");
+("Solo");
 
-INSERT INTO flytjandi (nafn, aldur, faedingardagur, Dánardagur)
+INSERT INTO flytjandi (nafn, aldur, faedingardagur, Dánardagur, ID_flokks)
 VALUES 
-("Coheed & Cambria", 22, 1995, NULL),
-("The Dear Hunter", 12, 2005, NULL),
-("Green Day", 31,1986, NULL),
-("Avenged Sevenfold", 18, 1999, NULL),
-("Coheed & Cambria", 22, 1995, NULL),
-("Thank you scientist", 8, 2009, NULL),
-("Pink Floyd", 49, 1965, 2014);
+("Coheed & Cambria", 22, 1995, NULL, 1),
+("The Dear Hunter", 12, 2005, NULL, 2),
+("Green Day", 31,1986, NULL, 1),
+("Avenged Sevenfold", 18, 1999, NULL, 1),
+("Coheed & Cambria", 22, 1995, NULL, 1),
+("Thank you scientist", 8, 2009, NULL, 1),
+("Pink Floyd", 49, 1965, 2014, 1);
 
 INSERT INTO lagahöfundur (nafn, aldur, faedingardagur, Dánardagur)
 VALUES 
@@ -107,48 +99,49 @@ VALUES
 ("Salvatore Marrano", 23, 12-7-1994, NULL),
 ("Roger Waters", 73, 6-9-1943, NULL);
 
-INSERT INTO diskur (nafn, utgafuar)
+INSERT INTO diskur (nafn, utgafuar, ID_tegundar, ID_utgefanda)
 VALUES 
-("Good Apollo I'm Burning Star IV Volume One. From Fear Through the Eyes of Madness", 2005),
-("Act IV. Rebirth in Reprise",2015),
-("American Idiot",2004),
-("The Stage",2016),
-("In Keeping Secrets of Silent Earth. 3",2003),
-("The wall", 1978);
+("Good Apollo I'm Burning Star IV Volume One. From Fear Through the Eyes of Madness", 2005, 1, 1),
+("Act IV. Rebirth in Reprise",2015, 2, 2),
+("American Idiot",2004, 3, 3),
+("The Stage",2016, 4, 4),
+("In Keeping Secrets of Silent Earth. 3",2003, 1, 1),
+("Maps of Non-Existent Places", 2012, 5, 5),
+("The wall", 1978, 5, 1);
 
 INSERT INTO lag (nafn, lengd, ID_disks, ID_hofundar, ID_flytjanda)
 VALUES
-("Welcome home", 6.14, 1, 1, 1),
-("Ten speed (Of god's blood and burial)",3.46, 1, 1, 1),
-("Apollo I. The writing writer", 5.15, 1, 1, 1),
-("The suffering", 3.43, 1, 1, 1),
+("Welcome home", 374, 1, 1, 1),
+("Ten speed (Of god's blood and burial)",226, 1, 1, 1),
+("Apollo I. The writing writer", 315, 1, 1, 1),
+("The suffering", 223, 1, 1, 1),
 
-("Waves", 4.12, 2, 2, 2),
-("The squeaky wheel", 4.35, 2, 2, 2),
-("King of swords (Reversed)", 5.07, 2, 2, 2),
-("If all goes well", 4.41, 2, 2, 2),
+("Waves", 252, 2, 2, 2),
+("The squeaky wheel", 275, 2, 2, 2),
+("King of swords (Reversed)", 307, 2, 2, 2),
+("If all goes well", 281, 2, 2, 2),
 
-("American Idiot", 2.54, 3, 3, 3),
-("Jesus of suburbia", 9.08, 3, 3, 3),
-("Holiday", 3.52, 3, 3, 3),
-("Wake me up when september ends", 4.45, 3, 3, 3),
+("American Idiot", 174, 3, 3, 3),
+("Jesus of suburbia", 548, 3, 3, 3),
+("Holiday", 232, 3, 3, 3),
+("Wake me up when september ends", 285, 3, 3, 3),
 
-("The Stage", 8.32, 4, 4, 4),
-("God damn", 3.41, 4, 4, 4),
-("Angels", 5.40, 4, 4, 4),
-("Exist", 15.41, 4, 4, 4),
+("The Stage", 512, 4, 4, 4),
+("God damn", 221, 4, 4, 4),
+("Angels", 540, 4, 4, 4),
+("Exist", 941, 4, 4, 4),
 
-("In Keeping Secrets of Silent Earth. 3", 8.13, 5, 5, 5),
-("Three evils", 5.11, 5, 5, 5),
-("The camper Velorium I. Faint of hearts", 5.22, 5, 5, 5),
-("A favor house atlantic", 3.53, 5, 5, 5),
+("In Keeping Secrets of Silent Earth. 3", 493, 5, 5, 5),
+("Three evils", 311, 5, 5, 5),
+("The camper Velorium I. Faint of hearts", 322, 5, 5, 5),
+("A favor house atlantic", 233, 5, 5, 5),
 
-("A Salesman's Guide to Non-Existence", 5.06, 6, 6, 6),
-("Feed the horses", 6.28, 6, 6, 6),
-("Blood on the Radio", 9.24, 6, 6, 6),
-("My Famed Disappearing Act", 5.44, 6, 6, 6),
+("A Salesman's Guide to Non-Existence", 306, 6, 6, 6),
+("Feed the horses", 388, 6, 6, 6),
+("Blood on the Radio", 564, 6, 6, 6),
+("My Famed Disappearing Act", 344, 6, 6, 6),
 
-("In the flesh", 4.15, 7, 7, 7),
-("Run like hell", 4.20, 7, 7, 7),
-("Another brick in the wall pt2", 3.49, 7, 7, 7),
-("Comfortly numb", 6.23, 7, 7, 7);
+("In the flesh", 255, 7, 7, 7),
+("Run like hell", 260, 7, 7, 7),
+("Another brick in the wall pt2", 229, 7, 7, 7),
+("Comfortly numb", 383, 7, 7, 7);
--- Lokaverkefni svör.sql
+++ Lokaverkefni svör.sql
@@ -9,7 +9,7 @@ where diskur.nafn like 'American Idiot';
 
 
 #C
-select lag.nafn as 'lag', tegund.nafn as 'lag' from diskur
+select lag.nafn as 'lag', tegund.nafn as 'tegund' from diskur
 join lag on diskur.ID = lag.ID_disks
 join tegund on diskur.ID_tegundar = tegund.ID
 where tegund.nafn like 'progressive rock';
@@ -43,38 +43,62 @@ group by count(diskur) DESC ;
 
 
 #K
-select
+select nafn from lag where nafn like 'A%';
+select nafn from lag where nafn like '%s%';
 
 #L
 
 
 #M
-select
+select avg(lengd) as 'lengd í sek' from lag; 
 
 #N
 
 
 #O
-select
+select flytjandi.faedingardagur, lag.nafn from lag
+join flytjandi on flytjandi.ID = lag.ID_flytjanda
+order by faedingardagur ASC
+limit 4;
 
 ######Okkar eigin fyrirspurnir######
 
 #1
-
+#Sýndu öll lög frá diskum sem byrja á 'The'
+select lag.nafn as lag, diskur.nafn as diskur from lag
+join diskur on diskur.ID = lag.ID_disks
+where diskur.Nafn like 'the%';
 
 #2
-
+#Sýndu lög alla flytjenda sem eru hættir/dánir
+select lag.nafn as lag, flytjandi.nafn as 'flytjandi', flytjandi.Dánardagur from lag
+join flytjandi on lag.ID_flytjanda = flytjandi.ID
+where flytjandi.Dánardagur is not null;
 
 #3
-
+#Sýndu öll lög sem eru í tegundinni 'metal'
+select lag.nafn, tegund.nafn from diskur
+join lag on diskur.ID = lag.ID_disks
+join tegund on tegund.ID = diskur.ID_tegundar
+where tegund.nafn like '%metal';
 
 #4
-
+#Sýndu alla diska sem hafa lag sem byrjar á 'The'
+select diskur.nafn as diskur from lag
+join diskur on diskur.ID = lag.ID_disks
+where lag.Nafn like 'the%' group by diskur;
 
 #5
+#Sýndu lög allra lagahöfunda sem hafa 3 nöfn í nafni sínu
+select lag.nafn, lagahöfundur.nafn from lag
+join lagahöfundur on lagahöfundur.ID = lag.ID_hofundar
+where lagahöfundur.nafn like '% % %';
 
 
 #6
+#
 
 
 #7
+#
+
